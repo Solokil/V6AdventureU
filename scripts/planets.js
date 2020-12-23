4976a25920adc7f97fef89b23c2648fb0adec046
Solokil <43015094+Solokil@users.noreply.github.com>
2020-12-23 00:37:45 -0300

    Delete planets.js

--- scripts/planets.js
+++
@@ -1,5 +0,0 @@
-const mercury = new JavaAdapter(Planet, {}, "mercury", Planets.sun, 4, 0.7);
-mercury.orbitRadius = 10.0;
-mercury.meshLoader = () => new SunMesh(mercury, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("202020"), Color.valueOf("161616"), Color.valueOf("101010"));
-mercury.accessible = false;
-mercury.hasAtmosphere = false;

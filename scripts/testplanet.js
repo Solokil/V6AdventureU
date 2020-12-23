92e142b79999d20849106716e269a83d3ad11c43
Solokil <43015094+Solokil@users.noreply.github.com>
2020-12-23 01:20:35 -0300

    Update testplanet.js

--- scripts/testplanet.js
+++ scripts/testplanet.js
@@ -1,12 +1,18 @@
+//Test planet 1
+
+const generator = extend(SerpuloPlanetGenerator, {});
+generator.dec = ObjectMap.of(Blocks.grass, Blocks.dirt, Blocks.grass, Blocks.dirt, Blocks.water, Blocks.water, Blocks.darksandWater, Blocks.darksandWater);
+generator.tars = ObjectMap.of(Blocks.grass, Blocks.shale, Blocks.dirt, Blocks.shale);
+
 const testplanet = new JavaAdapter(Planet, {}, "tesplanet", Planets.serpulo, 4, 0.7);
-testplanet.localizedName = "Red Moon base";
+testplanet.localizedName = "First Moon base";
 testplanet.orbitRadius = 4.0;
 testplanet.meshLoader = () => new SunMesh(testplanet, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("202020"), Color.valueOf("161616"), Color.valueOf("101010"));
 testplanet.accessible = true;
 testplanet.hasAtmosphere = true;
-testplanet.generator = new SerpuloPlanetGenerator();
+testplanet.generator = generator;
 testplanet.bloom = false;
-testplanet.radius = 1;
+testplanet.radius = 0.4;
 testplanet.accessible = true;
 testplanet.hasAtmosphere = true;
 testplanet.atmosphereColor = Color.valueOf("80ff00");

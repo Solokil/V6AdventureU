95043f71553f488aaa9edb7186a8a90f202b3ddb
Solokil <43015094+Solokil@users.noreply.github.com>
2020-12-23 00:41:43 -0300

    Update testplanet.js

--- scripts/testplanet.js
+++ scripts/testplanet.js
@@ -1,4 +1,8 @@
-
+const testplanet = new JavaAdapter(Planet, {}, "Moon base", Planets.serpulo, 4, 0.7);
+mercury.orbitRadius = 4.0;
+mercury.meshLoader = () => new SunMesh(mercury, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("202020"), Color.valueOf("161616"), Color.valueOf("101010"));
+mercury.accessible = true;
+mercury.hasAtmosphere = true;
 
 
 //Planet sector pressets

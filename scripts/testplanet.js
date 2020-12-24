a930d94d2618cea72fe4e41ea49fbc1909fb770d
Solokil <43015094+Solokil@users.noreply.github.com>
2020-12-24 20:53:11 -0300

    Update testplanet.js

--- scripts/testplanet.js
+++ scripts/testplanet.js
@@ -1,14 +1,13 @@
-//const node = require ('nodeFunction');
 //Test planet 1
 
 const generator = extend(SerpuloPlanetGenerator, {});
 generator.dec = ObjectMap.of(Blocks.grass, Blocks.dirt, Blocks.grass, Blocks.dirt, Blocks.water, Blocks.water, Blocks.darksandWater, Blocks.darksandWater);
 generator.tars = ObjectMap.of(Blocks.grass, Blocks.shale, Blocks.dirt, Blocks.shale);
 
-const testplanet = new JavaAdapter(Planet, {}, "tesplanet", Planets.serpulo, 1, 0.5); //4,0.7
+const testplanet = new JavaAdapter(Planet, {}, "tesplanet", Planets.serpulo, 0.4, 0.2); //4,0.7
 testplanet.localizedName = "First Moon base";
 testplanet.orbitRadius = 4.0;
-testplanet.meshLoader = () => new SunMesh(testplanet, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("f5d300"), Color.valueOf("74737a"), Color.valueOf("00aSe3"));
+testplanet.meshLoader = () => new SunMesh(testplanet, 4, 5, 0.3, 0.7, 0.6, 0.5, 1.5, Color.valueOf("f5d300"), Color.valueOf("74737a"), Color.valueOf("00aSe3"));
 testplanet.accessible = true;
 testplanet.hasAtmosphere = true;
 testplanet.generator = generator;
@@ -31,6 +30,15 @@ zonetest1.objectives = Seq.with(
   //new Objectives.Research(Vars.content.getByName(ContentType.block, "goldmod-goldForge")),
   //new Objectives.Research(Blocks.laserDrill)
 );
+const zone1 = Vars.content.getByName(ContentType.block, "V6AdventureU-zonetest1");
+newNode(zone1, groundZero, null, Seq.with(new Objectives.SectorComplete(SectorPresets.groundZero), new Objectives.Research(Blocks.launchPad)));
 
-//const zonetest1node = Vars.content.getByName(ContentType.sector, "V6AdventureU-zonetest1");
-//node.newNode(zonetest1node, groundZero, null, Seq.with(new Objectives.SectorComplete(SectorPresets.groundZero), new Objectives.Research(Blocks.V6AdventureU-0001-ironite-compress)));
+function newNode(content, parentName, req, objectives){
+  var parent = TechTree.all.find(t => t.content.name.equals(parentName));
+
+  var node = new TechTree.TechNode(parent, content, req);
+  node.objectives.add(objectives);
+  
+  parent.children.add(node);
+}
+

99fe691c35bc323b55c12c01ac963c27f8ea7d92
Solokil <43015094+Solokil@users.noreply.github.com>
2020-12-24 21:28:05 -0300

    Update testplanet.js

--- scripts/testplanet.js
+++ scripts/testplanet.js
@@ -4,10 +4,10 @@ const generator = extend(SerpuloPlanetGenerator, {});
 generator.dec = ObjectMap.of(Blocks.grass, Blocks.dirt, Blocks.grass, Blocks.dirt, Blocks.water, Blocks.water, Blocks.darksandWater, Blocks.darksandWater);
 generator.tars = ObjectMap.of(Blocks.grass, Blocks.shale, Blocks.dirt, Blocks.shale);
 
-const testplanet = new JavaAdapter(Planet, {}, "tesplanet", Planets.serpulo, 4, 0.8); //4,0.7
+const testplanet = new JavaAdapter(Planet, {}, "tesplanet", Planets.serpulo, 2, 0.8); //4,0.7
 testplanet.localizedName = "First Moon base";
 testplanet.orbitRadius = 4.0;
-testplanet.meshLoader = () => new SunMesh(testplanet, 4, 5, 0.3, 0.7, 0.6, 0.5, 1.5, Color.valueOf("f5d300"), Color.valueOf("74737a"), Color.valueOf("00aSe3"));
+testplanet.meshLoader = () => new SunMesh(testplanet, 4, 5, 0.3, 0.7, 0.6, 0.5, 0.4, Color.valueOf("f5d300"), Color.valueOf("74737a"), Color.valueOf("00aSe3"));
 testplanet.accessible = true;
 testplanet.hasAtmosphere = true;
 testplanet.generator = generator;
@@ -31,7 +31,9 @@ zonetest1.objectives = Seq.with(
   //new Objectives.Research(Blocks.laserDrill)
 );
 const zone1 = Vars.content.getByName(ContentType.sector, "V6AdventureU-zonetest1");
-newNode(zone1, groundZero, null, Seq.with(new Objectives.SectorComplete(SectorPresets.groundZero), new Objectives.Research(Blocks.launchPad)));
+   Load(){
+    newNode(zone1, groundZero, null, Seq.with(new Objectives.SectorComplete(SectorPresets.groundZero), new Objectives.Research(Blocks.launchPad)));
+   },
 
 function newNode(content, parentName, req, objectives){
   var parent = TechTree.all.find(t => t.content.name.equals(parentName));

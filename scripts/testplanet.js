0072ace306fe44667b6b3df0e1a29dbf0710c4ea
Solokil <43015094+Solokil@users.noreply.github.com>
2020-12-24 21:33:52 -0300

    Update testplanet.js

--- scripts/testplanet.js
+++ scripts/testplanet.js
@@ -7,7 +7,7 @@ generator.tars = ObjectMap.of(Blocks.grass, Blocks.shale, Blocks.dirt, Blocks.sh
 const testplanet = new JavaAdapter(Planet, {}, "tesplanet", Planets.serpulo, 2, 0.8); //4,0.7
 testplanet.localizedName = "First Moon base";
 testplanet.orbitRadius = 4.0;
-testplanet.meshLoader = () => new SunMesh(testplanet, 4, 5, 0.3, 0.7, 0.6, 0.5, 0.4, Color.valueOf("f5d300"), Color.valueOf("74737a"), Color.valueOf("00aSe3"));
+testplanet.meshLoader = () => new SunMesh(testplanet, 4, 5, 0.3, 1.8, 1.2, 0.5, 0.4, Color.valueOf("f5d300"), Color.valueOf("74737a"), Color.valueOf("00aSe3"));
 testplanet.accessible = true;
 testplanet.hasAtmosphere = true;
 testplanet.generator = generator;
@@ -23,16 +23,15 @@ const zonetest1 = new JavaAdapter(SectorPreset, {}, "zonetest1", Planets.testpla
 zonetest1.difficulty = 12;
 zonetest1.captureWave = 25;
 zonetest1.localizedName = "Flyn base";
-zonetest1.alwaysUnlocked = false;
+zonetest1.alwaysUnlocked = true;
 zonetest1.objectives = Seq.with(
   new Objectives.SectorComplete(SectorPresets.groundZero),
   new Objectives.Research(Vars.content.getByName(ContentType.block, "V6AdventureU-0001-ironite-compress")),
   //new Objectives.Research(Vars.content.getByName(ContentType.block, "goldmod-goldForge")),
   //new Objectives.Research(Blocks.laserDrill)
 );
-const zone1 = Vars.content.getByName(ContentType.sector, "V6AdventureU-zonetest1");
-   
-    newNode(zone1, groundZero, null, Seq.with(new Objectives.SectorComplete(SectorPresets.groundZero), new Objectives.Research(Blocks.launchPad)));
+//const zone1 = Vars.content.getByName(ContentType.sector, "V6AdventureU-zonetest1");
+// newNode(zone1, groundZero, null, Seq.with(new Objectives.SectorComplete(SectorPresets.groundZero), new Objectives.Research(Blocks.launchPad)));
    
 
 function newNode(content, parentName, req, objectives){

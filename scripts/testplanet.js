8e1fad8a197a9aae2cb2b4f33f32c2808eef91ca
Solokil <43015094+Solokil@users.noreply.github.com>
2020-12-26 14:34:43 -0300

    Update testplanet.js

--- scripts/testplanet.js
+++ scripts/testplanet.js
@@ -1,8 +1,8 @@
 //Test planet 1
-
+/*
 const generator = extend(SerpuloPlanetGenerator, {});
 generator.dec = ObjectMap.of(Blocks.grass, Blocks.dirt, Blocks.grass, Blocks.dirt, Blocks.water, Blocks.water, Blocks.darksandWater, Blocks.darksandWater);
-generator.tars = ObjectMap.of(Blocks.grass, Blocks.shale, Blocks.dirt, Blocks.shale);
+generator.tars = ObjectMap.of(Blocks.grass, Blocks.shale, Blocks.dirt, Blocks.shale);*/
 
 const testplanet = new Planet("testplanet", Planets.serpulo, 1, 0.5); //4, 0.7
 testplanet.localizedName = "First Moon base";
@@ -10,13 +10,13 @@ testplanet.orbitRadius = 4.0;
 testplanet.meshLoader = () => new SunMesh(testplanet, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("f5d300"), Color.valueOf("74737a"), Color.valueOf("00aSe3"));
 testplanet.accessible = true;
 testplanet.hasAtmosphere = true;
-testplanet.generator = generator;
-testplanet.bloom = false;
+//testplanet.generator = generator;
+testplanet.bloom = true;
 testplanet.radius = 1;
 testplanet.hasAtmosphere = true;
 testplanet.atmosphereColor = Color.valueOf("80ff00");
-testplanet.atmosphereRadIn = 0.1;
-testplanet.atmosphereRadOut = 0.2;
+testplanet.atmosphereRadIn = 0.02;
+testplanet.atmosphereRadOut = 0.3;
 
 //Planet sector pressets
 const zonetest1 = new SectorPreset("zonetest1", testplanet, 1);

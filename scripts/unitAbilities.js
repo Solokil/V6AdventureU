522d240d45d641c543b6aa3dd1a00a6be4ae5892
Solokil <43015094+Solokil@users.noreply.github.com>
2021-04-17 03:26:27 -0300

    Update unitAbilities.js

--- scripts/unitAbilities.js
+++ scripts/unitAbilities.js
@@ -7,10 +7,16 @@
 
 ///Ground Units///
 const gf1_tanker = extendContent(UnitType, "030035-iri-tanker", {});
-
 gf1_tanker.constructor = function(){
   return extend(PayloadUnit, {});
 };
 gf1_tanker.abilities.add(new ForceFieldAbility(30, 4, 100, 1200));
+
+
+const gf1_enraged_tanker = extendContent(UnitType, "220035-enraged-iri-tanker", {});
+gf1_enraged_tanker.constructor = function(){
+  return extend(PayloadUnit, {});
+};
+gf1_enraged_tanker.abilities.add(new ForceFieldAbility(30, 4, 100, 1200));
 
 ///End Ground Units///
